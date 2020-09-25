f950a2e9cfc7b695d58a9c5427324cad30ccc621
asari-matsuda <asari-matsuda@users.noreply.github.com>
2020-09-25 17:49:14 +0900

    Put together in set_room

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :set_room, only: [:index, :create]
+
   def index
     @message = Message.new
-    @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
   end
 
   def create
-    @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
@@ -18,5 +18,9 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_room
+    @room = Room.find(params[:room_id])
   end
 end
